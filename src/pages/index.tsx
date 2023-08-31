e8b037b298434ace791dc8c91fd6b442214e9075
LLTae2 <taehyun5820@gmail.com>
2023-08-31 10:36:07 +0900

    Create:: 소개 컴포넌트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import React, { FunctionComponent } from 'react'
-import Text from 'components/Text'
-import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/common/GlobalStyle'
+import Introduction from 'components/Main/Introduction'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
 
 const IndexPage: FunctionComponent = function () {
   return (
-    <div>
-      <Link to="/info">To Info</Link>
-    </div>
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+    </Container>
   )
 }
 
